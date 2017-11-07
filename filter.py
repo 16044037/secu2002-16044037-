00ccdacadbe3338695332fb0aa1288822aa030b1
16044037 <33095135+16044037@users.noreply.github.com>
2017-11-07 23:00:50 +0000

    Add files via upload

--- filter.py
+++ filter.py
@@ -21,13 +21,24 @@ print '------------'
 #Task 2
 
 #Here we want to use lambda to filter instead of defining a function to do so
-#I want to store the list as the new filtered list and so I have used a statement to do this
-mylist=filter(lambda x : x > 23, mylist)
+#I want to store the new list as the filtered list and so I have used a statement to do this
+#I am inputting a list (mylist) and a number (n)
+#By using their names rather than the values assigned to the names, it means that it can be used in various examples instead of being specific to just one
+#This means that I can change the value of mylist and n and still get the correct result (as shown in a second example below)
+mylist=filter(lambda x : x > n, mylist)
 #This will then test whether the new list has been sucessfully stored
 #It will also test whether the filter function has worked correctly
 print 'This should return 33 and 43'
 print 'Is returning\t',mylist
 
+print '------------'
 
+#Example 2
+mylist=[4,5,6,7,8]
+n = 6
 
+mylist=filter(lambda x : x > n, mylist)
+
+print 'This should return 7 and 8'
+print 'Is returning\t',mylist
 
